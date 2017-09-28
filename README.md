460939018a43d88163f1f8984a98d97de42e8469
jngermon <jn.germon@gmail.com>
2017-09-28 14:22:45 +0200

    Add script for php benchmark

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # utils
 Several scripts
+
+## PHP scripts
+
+### Test your PHP performances
+Just run this command to know how long is PHP for several loops
+```
+php time_for_php_loops.php {N}
+```
+Replace `{N}` by integer like '9' for 1 000 000 000 loops.
+ 
---
+++ time_for_php_loops.php
@@ -0,0 +1,28 @@
+<?php
+
+if ($argc < 2) {
+        print("First argument is missing (integer, 10^N loops).\n");
+}
+
+$pow = (int) $argv[1];
+
+if (!$pow || $pow < 1) {
+        print("The first argument have to be a positive integer\n");
+}
+
+$limit = pow(10, $pow);
+
+printf("Go for %d loops\n", $limit);
+
+$s = microtime(true);
+
+$i = 0;
+do {
+        $i++;
+} while($i < $limit);
+
+$e = microtime(true);
+$diff = DateTime::createFromFormat('U.u', round($e-$s, 6));
+
+echo $diff->format("H:i:s.u")."\n";
+
